6b8fe8334b59283e448ffd16da4edcd26c6c9546
nesalcedog <nataliaelinsg@gmail.com>
2026-05-22 19:59:37 -0300

    Creacion de la clase GestionVehiculo

--- SistemaGestiónTransporteTurístico/src/sistemagestióntransporteturístico/GestionVehiculo.java
+++ SistemaGestiónTransporteTurístico/src/sistemagestióntransporteturístico/GestionVehiculo.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package sistemagestióntransporteturístico;
 
 import java.util.ArrayList;
@@ -15,25 +12,67 @@ public class GestionVehiculo {
     private ArrayList<Vehiculo> vehiculos;
     
     public GestionVehiculo(){
-        vehiculos = new ArrayList<Vehiculo>();
+        vehiculos = new ArrayList<>();
     }
     
-    
+    //Metodo para mostrar Vehiculos ingresados en la lista
     public void mostrarVehiculo(){
         
         //Verificamos si existe un vehiculo cargado en la lista
-        if(vehiculos.isEmpty()){
-            System.out.println("No hay ningun vehiculo cargado.");
-            return;
-        }
+        verificacionArrayVacio(vehiculos);
         
         System.out.println("----- Lista de Vehiculos  -----");
         
         for(int i = 0; i < vehiculos.size(); i ++){
             
+            System.out.println(vehiculos.get(i).toString());                   
             
         }
         
     }
-    
+     
+    public void realizarServicioTuristico(){
+
+        verificacionArrayVacio(vehiculos);
+        
+        System.out.println("Verificando si puede hacer servicio turistico...");
+        for (int i = 0; i < vehiculos.size(); i++) {
+            Vehiculo vehiculoActual = vehiculos.get(i);
+            // Verificamos el vehiculo implementa servicioTuristico
+            if (vehiculoActual instanceof servicioTuristico) {
+                servicioTuristico siServicioTuristico = (servicioTuristico) vehiculoActual; //si pasa el if, sabemos que si, entonces lo casteo
+                siServicioTuristico.realizarServicioTuristico();
+            } else {
+                // Si no implementa, entonces no puede realizar el servicio
+                System.out.println("El vehiculo " + vehiculoActual.getMarca() + " no realiza servicios turisticos.");
+            }
+        }
+    }
+    }
+
+    public void mostrarVehiculosCapacidad(int capacidadMax) {
+        
+        verificacionArrayVacio(vehiculos);
+
+        System.out.println("----- Vehiculos con mayor capacidad a " + capacidadMax + "-----");
+
+        for (int i = 0; i < vehiculos.size(); i++) {
+
+            Vehiculo vehiculoActual = vehiculos.get(i);
+
+            if (vehiculoActual.getCapacidadPasajeros() > capacidadMax) {
+
+                System.out.println(vehiculoActual.getMarca() + "tiene una capacidad mayor a  " + vehiculoActual.getCapacidadPasajeros());
+            }
+
+        }
+
+    }
+
+    public void verificacionArrayVacio(ArrayList lista) {
+
+        if (lista.isEmpty()) {
+            System.out.println("No hay ningun vehiculo cargado.");
+            return;
+        }
 }
