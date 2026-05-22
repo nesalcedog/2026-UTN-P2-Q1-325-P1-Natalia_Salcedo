97b0ac7154b1912bcc9d0c61bddc6933d63eacc3
nesalcedog <nataliaelinsg@gmail.com>
2026-05-22 20:39:57 -0300

    Se agrega el Menu al Main

--- SistemaGestiónTransporteTurístico/src/sistemagestióntransporteturístico/Main.java
+++ SistemaGestiónTransporteTurístico/src/sistemagestióntransporteturístico/Main.java
@@ -1,17 +1,70 @@
 
 package sistemagestióntransporteturístico;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author Natu
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+    
+    private static Scanner scanner = new Scanner(System.in);
+    
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        int opcion;
+        do {
+            mostrarMenu();
+            
+            //Validamos que ingrese un numero
+            while (!scanner.hasNextInt()) {
+                System.out.println("Ingrese un número válido: ");
+                scanner.next();
+            }
+
+            opcion = scanner.nextInt();
+            scanner.nextLine(); //Para limpiar
+
+            switch (opcion) {
+                case 1: agregarVehiculo(); 
+                        break;                
+                //case 2: plataforma.listarFlota(); break;
+               // case 3: alquilarVehiculo(); break;
+                //case 4: finalizarAlquiler(); break;
+                //case 5: enviarACarga(); break;
+               // case 6: enviarAMantenimiento(); break;
+               // case 7: plataforma.listarVehiculosQueNecesitanCarga(); break;
+                case 8: System.out.println("¡Hasta luego!"); break;
+                default: System.out.println("Opción inválida.");
+            }
+
+        } while (opcion != 8);
+
+        scanner.close();
+    }
+    
+    static void mostrarMenu() {
+        System.out.println("----- MENU DE VEHICULOS -----");
+        System.out.println(" 1. Agregar vehiculo");
+        System.out.println(" 2. Mostrar todos los vehículos");
+        System.out.println(" 3. Realizar servicios turísticos");
+        System.out.println(" 4. Buscar vehículo por patente");
+        System.out.println(" 5. Mostrar vehículos con capacidad mayor a un valor ingresado");
+        System.out.println(" 6. Mostrar vehículos ordenados por año de fabricación descendente");
+        System.out.println(" 7. Mostrar vehículos ordenados por capacidad de pasajeros descendente");
+        System.out.println(" 8. Salir");
+        System.out.println("========================================");
+    }
+        
+    static void agregarVehiculo(){    
+        
+        System.out.println("Elija que tipo de vehiculo quiere agregar: ");
+        System.out.println("Elija que tipo de vehiculo quiere agregar: ");
+        
     }
     
 }
